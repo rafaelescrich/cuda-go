8ba0db60692ec4f64109927f4b422cb35728d7a4
Rafael Escrich <rafaelescrich@gmail.com>
2018-09-15 22:50:48 -0300

    Cleaning repo

--- cudaHello.go
+++ cudaHello.go
@@ -1,15 +1,17 @@
 package main
-import(
-	"github.com/barnex/cuda5/cu"
-	_ "github.com/mumax/3/cuda"
-	"fmt"
+
+import (
 	"C"
+	"fmt"
 	"strconv"
+
+	cu "github.com/mumax/3/cuda/cu"
 )
 
-func main(){
-	cu.Init(0);
-	fmt.Println("Hello, I am you GPU:", cu.Device(0).Name())
+func main() {
+	cu.Init(0)
+	fmt.Println("Hello, I am your GPU:", cu.Device(0).Name())
+	fmt.Println("Device's clock rate is: ", cu.Device(0).Properties().ClockRate)
 	fmt.Println("Number of devices: " + strconv.Itoa(cu.DeviceGetCount()))
-	fmt.Println("Free memory: " + strconv.FormatInt(cu.DeviceGet(0).TotalMem(),10))
+	fmt.Println("Free memory: " + strconv.FormatInt(cu.DeviceGet(0).TotalMem(), 10))
 }
